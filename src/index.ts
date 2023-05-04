230c12b851073991947af87b6c1a5c15e1ed468d
Ankur Seth <iankurseth@gmail.com>
2023-05-04 20:30:26 +0530

    Published coloria@1.0.4, exported TailwindColor type

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { fromColor, fromHex, fromRgb, getNeutral } from './colorfactory';
 export { hexToRgb, rgbToHex } from './colorutils';
 export { htmlcolors } from './htmlcolors';
 export { sanitizeCodeName } from './utils';
+export { TailwindColor } from './tailwindcolor';
